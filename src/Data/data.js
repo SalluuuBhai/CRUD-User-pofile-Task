06793255c1d00d0b2093f6be0e6d280fd13e8baa
SalluuuBhai <md.salmanfaris03@gmail.com>
2023-04-25 22:06:38 +0530

    first commit

---
+++ src/Data/data.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    id: "100",
+    name: "Salman",
+    batch: "B43WD",
+    email: "salman@gmail.com",
+    experience: 2,
+  },
+  {
+    id: "101",
+    name: "Azeem",
+    batch: "B43WD",
+    email: "azeem@gmail.com",
+    experience: 1,
+  },
+  {
+    id: "102",
+    name: "Sabeeth",
+    batch: "B43WD",
+    email: "sabeeth@gmail.com",
+    experience: 1,
+  },
+  {
+    id: "103",
+    name: "Sriram",
+    batch: "B43WD",
+    email: "sriram@gmail.com",
+    experience: 3,
+  },
+  {
+    id: "104",
+    name: "Asar",
+    batch: "B43WD",
+    email: "asar@gmail.com",
+    experience: 2,
+  },
+  {
+    id: "105",
+    name: "Shahruk",
+    batch: "B43WD",
+    email: "shahruk@gmail.com",
+    experience: 1,
+  },
+];
